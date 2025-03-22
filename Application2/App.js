69219b72c7a330a9cb4cfd33e227d6ebe322f5b2
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-03-22 10:22:26 +0530

    project loaded

---
+++ Application2/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { FirebaseProvider } from './context/firebase';
+import Form from './components/Form.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <StatusBar style='light' /> */}
+      <FirebaseProvider>
+        <Form />
+      </FirebaseProvider>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
