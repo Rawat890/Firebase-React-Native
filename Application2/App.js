8db5c64bf3e117efb01d29e4fd45997d2c4087bc
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-03-22 11:08:36 +0530

    Form component called and firebase Hook used to access utility functions

--- Application2/App.js
+++ Application2/App.js
@@ -1,25 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { FirebaseProvider } from './context/firebase';
 import Form from './components/Form.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <StatusBar style='light' /> */}
       <FirebaseProvider>
         <Form />
       </FirebaseProvider>
-
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
